6b172d85a4160d6c6b42ff78e0f565325637d525
Oshadhie Indudunu <153111583+Oshadhie@users.noreply.github.com>
2024-06-23 08:41:51 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import './App.css'
 import Home from './pages/Home';
+import Success from './pages/Success';
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
-        
+          <Route path='/success' element={<Success/>}/>
         </Routes>
         <Toaster/>
       </Router>
